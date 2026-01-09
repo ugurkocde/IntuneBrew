75e47f92b7ab729ed4d0bf97c590061b402fdcf6
GitHub Action <action@github.com>
2026-01-09 00:49:06 +0000

    Generated      417 uninstall scripts

--- Uninstall Scripts/uninstall_imazing.sh
+++ Uninstall Scripts/uninstall_imazing.sh
@@ -17,13 +17,13 @@ fi
 echo "Stopping iMazing if running..."
 pkill -f "iMazing" 2>/dev/null || true
 
-# Kill application with bundle ID com.DigiDNA.iMazing3.4.0.23277Mac if running
-echo "Stopping application with bundle ID com.DigiDNA.iMazing3.4.0.23277Mac if running..."
-killall -9 "com.DigiDNA.iMazing3.4.0.23277Mac" 2>/dev/null || true
+# Kill application with bundle ID com.DigiDNA.iMazing3.4.0.23279Mac if running
+echo "Stopping application with bundle ID com.DigiDNA.iMazing3.4.0.23279Mac if running..."
+killall -9 "com.DigiDNA.iMazing3.4.0.23279Mac" 2>/dev/null || true
 
-# Kill application with bundle ID com.DigiDNA.iMazing3.4.0.23277Mac.Mini if running
-echo "Stopping application with bundle ID com.DigiDNA.iMazing3.4.0.23277Mac.Mini if running..."
-killall -9 "com.DigiDNA.iMazing3.4.0.23277Mac.Mini" 2>/dev/null || true
+# Kill application with bundle ID com.DigiDNA.iMazing3.4.0.23279Mac.Mini if running
+echo "Stopping application with bundle ID com.DigiDNA.iMazing3.4.0.23279Mac.Mini if running..."
+killall -9 "com.DigiDNA.iMazing3.4.0.23279Mac.Mini" 2>/dev/null || true
 
 # Remove /Applications/iMazing.app
 echo "Removing /Applications/iMazing.app..."
