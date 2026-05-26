0157d16c03c39862449a28e480af80a024f1357e
GitHub Action <action@github.com>
2026-05-26 13:11:14 +0000

    Add Support App to supported apps list

--- .github/scripts/collect_app_info.py
+++ .github/scripts/collect_app_info.py
@@ -484,6 +484,7 @@ app_urls = [
     "https://formulae.brew.sh/api/cask/affinity.json",
     "https://formulae.brew.sh/api/cask/meta.json",
     "https://formulae.brew.sh/api/cask/sipgate.json",
+    "https://formulae.brew.sh/api/cask/support.json",
 ]
 
 # DMG
